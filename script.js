9967ffa0e641c0d1cbdfffe690a78eb83541f029
AntonBeYt <143318322+AntonBeYt@users.noreply.github.com>
2023-11-28 13:06:00 +0100

    added images for slideshow, first videos

--- script.js
+++ script.js
@@ -49,7 +49,6 @@ const arrowRight = document.getElementById("slide-arrow-right");
 const arrowLeft = document.getElementById("slide-arrow-left");
 const bikeCity = document.getElementById("bike-city");
 const bikeForest = document.getElementById("bike-forest");
-const heroVideo = document.getElementById("hero-bike");
 
 const changeSlide = () => {
   arrowLeft.classList.toggle("hidden");
@@ -67,8 +66,71 @@ toTop.addEventListener("click", () => {
 });
 const logo = document.getElementById("logo");
 
-let scrollPosition;
+const slide1 = document.getElementById("bag1");
+const slide2 = document.getElementById("bag2");
+const slide3 = document.getElementById("bag3");
+const slide4 = document.getElementById("bottle1");
+const slide5 = document.getElementById("bottle3");
+const slide6 = document.getElementById("bottle4");
+const slide7 = document.getElementById("brillor3");
+const slide8 = document.getElementById("brillor4");
+const slide9 = document.getElementById("brillor5");
+const slide10 = document.getElementById("brillor6");
+const slide11 = document.getElementById("brillor7");
+const slide12 = document.getElementById("bagEndImg");
 
+const slideshowSrc = () => {
+  slide1.setAttribute(
+    "src",
+    "./assets/product-images-500/Bag1_Medbakgrund_500px.png"
+  );
+  slide2.setAttribute(
+    "src",
+    "./assets/product-images-500/Bag2_Medbakgrund_500px.png"
+  );
+  slide3.setAttribute(
+    "src",
+    "./assets/product-images-500/Bag3_Medbakgrund_500px.png"
+  );
+  slide4.setAttribute(
+    "src",
+    "./assets/product-images-500/Bottle_1_MedBakgrund_500.png"
+  );
+  slide5.setAttribute(
+    "src",
+    "./assets/product-images-500/Bottle_3_MedBakgrund_500.png"
+  );
+  slide6.setAttribute(
+    "src",
+    "./assets/product-images-500/Bottle_4_MedBakgrund_500.png"
+  );
+  slide7.setAttribute(
+    "src",
+    "./assets/product-images-500/Brillor3_Flygande_500.png"
+  );
+  slide8.setAttribute(
+    "src",
+    "./assets/product-images-500/Brillor4_Flygande_500.png"
+  );
+  slide9.setAttribute(
+    "src",
+    "./assets/product-images-500/Brillor5_Flygande_MedFodral_500.png"
+  );
+  slide10.setAttribute(
+    "src",
+    "./assets/product-images-500/Brillor6_Flygande_MedFodral_500.png"
+  );
+  slide11.setAttribute(
+    "src",
+    "./assets/product-images-500/Brillor7_Sida_logga_500.png"
+  );
+  slide12.setAttribute(
+    "src",
+    "./assets/product-images-500/Bag1_Medbakgrund_500px.png"
+  );
+};
+
+let scrollPosition;
 const getWindowWidth = function () {
   if (window.innerWidth > 1000) {
     scrollPosition = 2000;
@@ -79,6 +141,7 @@ const getWindowWidth = function () {
       "src",
       "./assets/bike-images/Countryside_Desktop.png"
     );
+    slideshowSrc();
   } else {
     scrollPosition = 2300;
   }
@@ -87,24 +150,40 @@ const getWindowWidth = function () {
 
 getWindowWidth();
 
+const heroVideo = document.getElementById("hero-bike");
+const detailsVideo = document.getElementById("details-bike");
 const width = window.matchMedia("(min-width: 1000px)");
-const source = document.createElement("source");
-source.setAttribute("type", "video/mp4");
-heroVideo.appendChild(source);
+const heroSource = document.createElement("source");
+const detailsSource = document.createElement("source");
+heroSource.setAttribute("type", "video/mp4");
+detailsSource.setAttribute("type", "video/mp4");
+heroVideo.appendChild(heroSource);
+detailsVideo.appendChild(detailsSource);
 
 if (width.matches) {
   heroVideo.pause();
-  source.removeAttribute("src");
-  source.setAttribute("src", "./assets/video/Hero_Turn_Desk_mp4.mp4");
+  heroSource.removeAttribute("src");
+  heroSource.setAttribute("src", "./assets/video/Hero_Turn_Desk_mp4.mp4");
   heroVideo.load();
   heroVideo.play();
+  detailsVideo.pause();
+  detailsSource.removeAttribute("src");
+  detailsSource.setAttribute("src", "./assets/video/Details_Desktop.mp4");
+  detailsVideo.load();
+  detailsVideo.play();
 } else {
   heroVideo.pause();
-  source.removeAttribute("src");
-  source.setAttribute("src", "./assets/video/Hero_Turn_Mobile_mp4.mp4");
+  heroSource.removeAttribute("src");
+  heroSource.setAttribute("src", "./assets/video/Hero_Turn_Mobile_mp4.mp4");
   heroVideo.load();
   heroVideo.play();
+  detailsVideo.pause();
+  detailsSource.removeAttribute("src");
+  detailsSource.setAttribute("src", "./assets/video/Details_Mobile.mp4");
+  detailsVideo.load();
+  detailsVideo.play();
 }
+
 /*
 const setAttribute = function () {
   if (window.innerWidth > 1000) {
